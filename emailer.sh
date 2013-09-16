82cbb7db44772d64baa16935f846a14c83fca30b
Mahesh Naidu <mahesh@mahesh-Dell-System-Inspiron-N4110>
2013-09-16 23:14:40 +0530

    Fix Errors Email sending

--- emailer.sh
+++ emailer.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-
 prevminute=$(date '+%b %d %H:%M' -d "1 minutes ago")
 echo "$prevminute"
 
 subject='Processes Monitoring'
-body=$(grep $prevminute /var/log/syslog | grep "Monitor")
 emailto=''
 
-$(echo $body | mail -s $subject $emailto)
 
-
+grep "$prevminute" /var/log/syslog | grep "Monitor" > /tmp/monitoremail.txt
+cat /tmp/monitoremail.txt | mail -s $subject $emailto
